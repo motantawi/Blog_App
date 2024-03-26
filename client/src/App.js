e456836b77b0c9d0972d14048a4b2c15dc96161a
motantawi <mostafa50.hh@gmail.com>
2024-03-26 02:44:16 +0200

    added fallback Loader to let fix data still loading

--- client/src/App.js
+++ client/src/App.js
@@ -3,76 +3,62 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import CssBaseline from "@mui/material/CssBaseline";
 import RequireAuth from "./components/RequireAuth";
-import { useAuth } from "./context/authContext";
-import RequireLogout from "./components/RequireLogout";
 import Profile from "./pages/User";
 import CreatePost from "./pages/Posts/CreatePost";
 import Home from "./pages/Home";
 import PostDetails from "./pages/Posts/PostDetails";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import { Suspense } from "react";
+import Loading from "src/components/Loading.jsx";
 
 const App = () => {
-  const auth = useAuth();
-
   return (
     <>
       <CssBaseline />
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <RequireAuth>
-                <Home />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/createpost"
-            element={
-              <RequireAuth>
-                <CreatePost />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/profile"
-            element={
-              <RequireAuth>
-                <Profile />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/register"
-            element={
-              <RequireLogout>
-                <SignUp />
-              </RequireLogout>
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <RequireLogout>
-                <SignIn />
-              </RequireLogout>
-            }
-          />
-          <Route
-            path="/postDetails/:id"
-            element={
-              <RequireAuth>
-                <PostDetails />
-              </RequireAuth>
-            }
-          />
+      <Suspense fallback={<Loading />}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <RequireAuth>
+                  <Home />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/createpost"
+              element={
+                <RequireAuth>
+                  <CreatePost />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <RequireAuth>
+                  <Profile />
+                </RequireAuth>
+              }
+            />
+            <Route path="/register" element={<SignUp />} />
+            <Route path="/login" element={<SignIn />} />
+            <Route
+              path="/postDetails/:id"
+              element={
+                <RequireAuth>
+                  <PostDetails />
+                </RequireAuth>
+              }
+            />
 
-          {auth.user && <Route path="*" element={<Home />} />}
-        </Routes>
-      </Router>
+            <Route path="*" element={<Home />} />
+          </Routes>
+        </Router>
+      </Suspense>
     </>
   );
 };
